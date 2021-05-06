65adecc1fdbdd64a16d36c46003b758cf77ed3dd
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2021-05-05 21:47:40 -0500

    Changed Database connection

--- config/Database.php
+++ config/Database.php
@@ -3,19 +3,24 @@
     // Create a database connection using a class
     class Database {
         // Database parameters
-        private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	';
-        private $db_name = 'scif14nniu3kzuc5';
-        private $username = 'dq2vnt8sgb11rgn8';
-        private $password = 'kkqkxaorr213zyhy';
         private $conn;
 
         // Database connection using a try/catch block
         public function connect () {
             // Set connection parameter to null
             $this->conn = null;
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
+            
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass'];
+            $database = ltrim($dbparts['path'],'/');
+
+            $dsn = "mysql:host=($hostname);dbname=($database)";
 
             try {
-                $this->conn = new PDO ('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO($dsn, $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOExecption $e) {
                 echo 'Connection Error: ' .$e->getMessage();

1849a70f46514a6358cdbae27cf6093f33d73c6c
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2021-05-08 21:38:13 -0500

    Change database variables

--- config/Database.php
+++ config/Database.php
@@ -3,6 +3,10 @@
     // Create a database connection using a class
     class Database {
         // Database parameters
+        // private $host = 'localhost';
+        // private $db_name = 'quotesdb';
+        // private $username = 'root';
+        // private $password = 'sesame';
         private $conn;
 
         // Database connection using a try/catch block
@@ -17,8 +21,12 @@
             $password = $dbparts['pass'];
             $database = ltrim($dbparts['path'],'/');
 
+            $dsn = "mysql:host={$hostname};dbname={$database}";
+
             try {
-                $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                // $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                // $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO($dsn, $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOExecption $e) {
                 echo 'Connection Error: ' .$e->getMessage();

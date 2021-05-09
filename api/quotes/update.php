6a7594b732239414fa7af43eb56cdcd1b90840bc
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2021-05-08 21:24:55 -0500

    Fixed update page

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -1,32 +1,35 @@
 <?php
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: PUT');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+require '../../config/Database.php';
+require '../../models/Quote.php';
 
-    require('../../config/Database.php');
-    require('../../models/Quote.php');
+$database = new Database();
+$db = $database->connect();
 
-    $database = new Database();
-    $db = $database->connect();
+$quote = new Quote($db);
 
-    $quote = new Quote($db);
+$data = json_decode(file_get_contents("php://input"));
 
-    $data = json_decode(file_get_contents('php://input'));
+$quote->quote = $data->quote;
+$quote->authorId = $data->authorId;
+$quote->categoryId = $data->categoryId;
+$quote->id = $data->id;
 
-    $quote->categoryId = $data->categoryId;
-    $quote->authorId = $data->authorId;
-    $quote->quote = $data->quote;
-    $quote->id = $data->id;
-
-    if(!empty($quote->quote) && !empty($quote->authorId)  && !empty($quote->cateoryId) && !empty($quote->id)) {
-        $quote->update();
-        echo json_encode(
-            array('message' => 'Quote Updated!')
-        );
-    } else {
-        echo json_encode(
-            array('message' => 'Quote did not update.')
-        );
-    }
+if(!empty($quote->quote) &&
+!empty($quote->authorId) &&
+!empty($quote->categoryId) &&
+!empty($quote->id)) {
+    $quote->update();
+    echo json_encode(
+        array('message' => 'Quote updated')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Quote was not updated')
+    );
+}
